6aebdab798a89bae9af11675f9dba4ffe2059577
Dag Jomar Mersland <dagjomar@gmail.com>
2026-05-05 22:51:22 +0200

    initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+// Milestone 1: Norwegian only (ingen tospråklig struktur ennå).
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = 'TRD_FRIES · Trondheim frites';
+export const SITE_DESCRIPTION =
+	'Kuratert arkiv og guide til Trondheims sprøeste, salteste og mest kompromissløse pommes frites.';
