b770e8703a405fc3579fd62b9ba1fbfd08efe26d
Dag Jomar Mersland <dagjomar@gmail.com>
2026-05-08 01:20:47 +0200

    Add new images for Hero section and Krambua venue; update Hero image reference and Leaderboard section text for clarity

--- src/data/venues.ts
+++ src/data/venues.ts
@@ -15,8 +15,7 @@ export type Venue = {
 const img =
 	'https://lh3.googleusercontent.com/aida-public/AB6AXuDLA3x6uVpOLR8WSS_zzlWppn3uKgaJuvlK7mUfTylKRZSenYTUpIucW_7pp1MsAnxSUXo5jkzKP-Vm4aRig67p57gqalzp-AXBs9mbz4JdXTRk0dNTSbMq8dkt29tWAJ3vU12aEF5v3KvEwjoRrMzgUPLTwn7CQNryujXd8hugth5ma1CwM5-8Wf9lAanomPz3_5ht8JbXFAAR5pzZIWRhqxFBhb6hOYIWM-DNQo0I5u0Kk3iEPVDOGj5p7cDCKg_9Y9jM9SIbPA';
 
-const imgHero =
-	'https://lh3.googleusercontent.com/aida-public/AB6AXuDjRHok5M5gQtZKuLfo8nzySxtfwO6B_20ixRuKv5DRzMLc8rxLAazhCPTeno-qzCIaa9cEiJ8Z6viLnl2-R6lhUepNuu-8_vpL3otK_Px0N2kIJEFm4RKbJ3I8Ye4FfMOKmLbFKMSojEorHC9jlJRLkHzjjAUPOBp1-RSTVGhVCk_Ol6Hd5Hx7YIn8XOUKnscrd0LMKVGeKePyxV5Gn6ZlclVCWbhr3cjs1IVkRV9DWcqtPeb672vDw1FEsIuH0o43oTZgoB2syw';
+const imgKrambua = '/images/restaurants/krambua/snitzel-721612.jpg';
 
 export const venues: Venue[] = [
 	{
@@ -26,7 +25,7 @@ export const venues: Venue[] = [
 		blurb:
 			'Krambugata gav en utrolig overraskende herlig opplevelse. Perfekt stekt, god smak, hint av potetskall, lagd fra bunnen av! Passe størrelse på en porsjon.',
 		traits: 'HJEMMELAGET · SENTRUM',
-		image: imgHero,
+		image: imgKrambua,
 		mapUrl:
 			'https://www.google.com/maps/search/?api=1&query=Krambugata+12+7010+Trondheim',
 		websiteUrl: 'https://www.krambuatrondheim.no/',
